0ac264e6e90e4af5dda3cffa7d5d5d90dc5607ae
Hridya Agrawal <hridayahoney@gmail.com>
2023-10-31 22:12:34 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -4,6 +4,3 @@ import mindsdb_sdk
 server = mindsdb_sdk.connect('http://127.0.0.1:47334')
 
 project = server.get_project()
-
-summarizer = project.models.get('cohere_text_summarization')
-generator = project.models.get('cohere_text_generation')
